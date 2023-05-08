8f036e15794933a0a520ffb7a84ed53c063e8180
Ezmant <ezmant14@gmail.com>
2023-05-07 23:41:35 -0300

    cart merge

---
+++ Frontend/animalesCommerce/src/app/pages/cart/cart.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+}
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent {
+  cartItems: Product[] = [];
+
+  addToCart(product: Product) {
+    this.cartItems.push(product);
+  }
+
+  removeFromCart(product: Product) {
+    const index = this.cartItems.findIndex((item) => item.id === product.id);
+    if (index > -1) {
+      this.cartItems.splice(index, 1);
+    }
+  }
+
+  getTotal() {
+    return this.cartItems.reduce((total, product) => total + product.price, 0);
+  }
+}
+
