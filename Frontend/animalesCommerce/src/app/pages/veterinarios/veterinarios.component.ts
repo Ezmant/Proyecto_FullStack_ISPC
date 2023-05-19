bcbb1172e9b2bf96f48f5c5274e9a7ec60d25953
Jazmin Amato <proyectos.amato@gmail.com>
2023-05-18 21:04:44 -0300

    Se agrego servicio para componente Veterinarios

--- Frontend/animalesCommerce/src/app/pages/veterinarios/veterinarios.component.ts
+++ Frontend/animalesCommerce/src/app/pages/veterinarios/veterinarios.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ListadoVeterinariosService } from 'src/app/servicios/listado-veterinarios.service';
 
 @Component({
   selector: 'app-veterinarios',
   templateUrl: './veterinarios.component.html',
   styleUrls: ['./veterinarios.component.css']
 })
-export class VeterinariosComponent {
+export class VeterinariosComponent implements OnInit{
 
+  lista: any;
+
+  constructor(private listVet:ListadoVeterinariosService) { }
+
+  ngOnInit(): void {
+    this.lista = this.listaVeterinarios();
+  }
+
+  listaVeterinarios():any {
+    this.listVet.verVeterinarios().subscribe({
+      next: (response) => {
+        this.lista = response
+      },
+      error: (errorResponse) => {
+        console.error(errorResponse)
+      }
+    })
+  }
+  
 }
