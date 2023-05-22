141e4530982f463078a591dc7ef3073e5caea807
Ezmant <ezmant14@gmail.com>
2023-05-21 23:06:04 -0300

    em

--- Frontend/animalesCommerce/src/app/pages/agradecimientos/agradecimientos.component.ts
+++ Frontend/animalesCommerce/src/app/pages/agradecimientos/agradecimientos.component.ts
@@ -1,6 +1,4 @@
-import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { AgradecimientosService } from 'src/app/services/agradecimientos.service';
 
 @Component({
   selector: 'app-agradecimientos',
@@ -9,22 +7,4 @@ import { AgradecimientosService } from 'src/app/services/agradecimientos.service
 })
 export class AgradecimientosComponent {
 
-  datos:any;
- constructor (private listPersonas: AgradecimientosService){}
-
-ngOnInit():void{
-  this.verAgradecimientos();
-}
-
- verAgradecimientos():void{
-  this.listPersonas.getAgradecimientos().subscribe({
-    next:(response)=>(
-      this.datos=response
-       //console.log(this.datos)
-      ),
-    error: (errorResponse)=> (
-      console.error(errorResponse)
-    )
-  })
- }
 }
