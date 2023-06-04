bedf1111a591140f74c395e98978a398817adb42
Jazmin Amato <proyectos.amato@gmail.com>
2023-06-04 00:40:18 -0300

    Se creo api de registro, login y logout de usuarios

--- Backend/abm_ispc/Aspra/models.py
+++ Backend/abm_ispc/Aspra/models.py
@@ -1,4 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(max_length=150, unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'password']
 
 class Refugio(models.Model):
     id=models.AutoField(primary_key=True)
